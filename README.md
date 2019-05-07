87e7f1349827506b2ffa732b8c7ac7ab6858d92a
Cameron Brown <hello@cameronbrown.co.uk>
2019-05-07 22:00:39 +0100

    Implemented masthead, first section work

---
+++ README.md
@@ -0,0 +1,13 @@
+# Decoupage Dreams
+## Goals
+* Market products and services (venue dressing & stationery)
+* Collect, manage and send emails automatically via Google Sheets & Gmail
+
+## Design documents
+* Landing page design doc
+
+## Debugging
+todo
+
+## Deploying
+todo
--- web/app.js
+++ web/app.js
@@ -1,2 +1,8 @@
 import { MDCRipple } from '@material/ripple/index';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import { MDCTopAppBar } from '@material/top-app-bar';
+// const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+// const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+import { MDCTextField } from '@material/textfield';
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
