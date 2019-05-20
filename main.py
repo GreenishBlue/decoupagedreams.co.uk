888b41b593d93715e9807ede0d86c47d3edecebf
Cameron Brown <hello@cameronbrown.co.uk>
2019-05-20 18:22:06 +0100

    Added Flask proxy server to Webpack bundle stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from requests import get
 
 
 app = Flask(__name__)
@@ -6,8 +7,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('index.html')
+
+
+@app.route('/build/bundle.css')
+def build_css():
+  """Request compiled CSS. This is only for development, and will be
+  overridden by the Google App Engine config.
+  
+  Local Webpack server must be running via `npm start`"""
+  return Response(get('http://localhost:8080/build/bundle.css').content, 
+                  mimetype='text/css')
+
+
+@app.route('/build/bundle.js')
+def build_js():
+  """Request compiled JS. This is only for development, and will be
+  overridden by the Google App Engine config.
+  
+  Local Webpack server must be running via `npm start`"""
+  return Response(get('http://localhost:8080/build/bundle.js').content, 
+                  mimetype='application/javascript')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
