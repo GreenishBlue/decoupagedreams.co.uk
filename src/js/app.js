2f3c7e28a05a5ccd92731df4a3b6156e0e1e61fa
Cameron Brown <hello@cameronbrown.co.uk>
2019-05-28 23:28:28 +0100

    Added initial serviceworker.. work

--- src/js/app.js
+++ src/js/app.js
@@ -24,6 +24,17 @@ class App {
     textFields.forEach((field) => {
       const textField = new MDCTextField(field);
     });
+
+    // Register service worker.
+    if ('serviceWorker' in navigator) {
+      console.log("Attempting to reigster service worker.");
+      navigator.serviceWorker.register('static/service-worker.js')
+        .then(function(reg){
+          console.log("Registered service worker!");
+       }).catch(function(err) {
+          console.log("Failed to register service worker:", err)
+      });
+    }
   }
 }
 
