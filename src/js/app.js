4d3822d89382b879e3542f1c8310e592f5973ab1
Cameron Brown <hello@cameronbrown.co.uk>
2019-06-20 01:41:18 +0100

    Added base.html & remove unused classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,4 @@
 // Entrypoint for the application.
-import { MDCRipple } from '@material/ripple/index';
 import { MDCTextField } from '@material/textfield';
 
 class App {
@@ -8,19 +7,7 @@ class App {
   }
 
   onStart() {
-    console.log('onStart called');
-
-    const mastheadContainer = document.querySelector('.data-masthead');
-
-    const appShell = document.querySelector('.data-app-shell');
-
-    // Setup navigation event listeners.
-    const navLinks = document.querySelectorAll('.data-href-spa');
-    navLinks.forEach((link) => {
-      console.log('Binding link: ' + link.href);
-    });
-
-    const textFields = document.querySelectorAll('.mdc-text-field'); 
+    const textFields = document.querySelectorAll('.mdc-text-field');
     textFields.forEach((field) => {
       const textField = new MDCTextField(field);
     });
@@ -29,17 +16,14 @@ class App {
     if ('serviceWorker' in navigator) {
       console.log("Attempting to reigster service worker.");
       navigator.serviceWorker.register('static/service-worker.js')
-        .then(function(reg){
+        .then(function (reg) {
           console.log("Registered service worker!");
-       }).catch(function(err) {
+        }).catch(function (err) {
           console.log("Failed to register service worker:", err)
-      });
+        });
     }
   }
 }
 
 window.app = new App();
 (() => app.onStart())();
-
-
-
