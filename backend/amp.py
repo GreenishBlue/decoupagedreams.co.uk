1a1a530b9039baa9edab39802728ad2e6f00beff
Cameron Brown <hello@cameronbrown.co.uk>
2019-07-08 18:20:33 +0100

    MORE AMP WORK

--- backend/amp.py
+++ backend/amp.py
@@ -1,8 +1,24 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from htmlmin.main import minify
 
 
 amp = Blueprint('amp', __name__, template_folder='templates')
+
+
+@amp.after_request
+def response_minify(response):
+  """
+  minify html response to decrease site traffic
+  """
+  if response.content_type == u'text/html; charset=utf-8':
+    response.set_data(
+      minify(response.get_data(as_text=True))
+    )
+
+    response.data = response.data.replace(b'<style>', b'<style amp-custom>')
+    return response
+  return response
 
 
 @amp.route('/', defaults={'page': 'index'})
